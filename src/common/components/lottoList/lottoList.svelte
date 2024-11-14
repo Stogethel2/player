9494df8a0002263fda10a180dcfce35b525d27ee
mali <nutcom3@gmail.com>
2024-11-14 17:35:46 +0700

    Done: lotto list

--- src/common/components/lottoList/lottoList.svelte
+++ src/common/components/lottoList/lottoList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { lottoApi } from "$lib/api/endpoint/lotto";
   import CardLotto from "../cardLotto/cardLotto.svelte";
   import { onMount } from "svelte";
   let playlist: any;
@@ -8,13 +9,26 @@
 
   onMount(async () => {
     try {
-      let response = null;
+      let lottoData;
       if (category === "yeekee") {
-        response = await fetch("/setting/yeekeelist.json"); // ดึงข้อมูลจาก static/settings.json
+        lottoData = await fetch("/setting/yeekeelist.json"); // ดึงข้อมูลจาก static/settings.json
       } else {
-        response = await fetch("/setting/playlist.json"); // ดึงข้อมูลจาก static/settings.json
+        lottoData = await lottoApi.getActiveLottos();
       }
-      playlist = await response.json();
+
+      playlist = lottoData;
+      const colors = [
+        "bg-red-500",
+        "bg-gray-500",
+        "bg-orange-500",
+        "bg-green-500",
+        "bg-blue-500",
+      ];
+      playlist = playlist.map((card: any) => {
+        card.header_color_class =
+          colors[Math.floor(Math.random() * colors.length)];
+        return card;
+      });
     } catch (error) {
       console.error("Error fetching settings:", error);
     } finally {
@@ -29,17 +43,17 @@
   {#if isLoading}
     <p>Loading...</p>
   {:else}
-    {#each playlist.list as card}
+    {#each playlist as card}
       <CardLotto
         headerColorClass={card.header_color_class}
-        headerImageBackground={card.header_image_background}
-        icon={card.icon}
-        title={card.title}
-        countDownText={card.time}
-        details={card.detail}
-        open={card.open}
-        name={card.name}
-        agent={card.agent}
+        headerImageBackground={"https://images.squarespace-cdn.com/content/v1/58cd025a4402439656dae04f/1561314690504-Y25AJHYE86T7YBYI3VV0/Reis+naar+Thailand+boeken-3.JPG"}
+        icon={"https://cdn-icons-png.flaticon.com/512/630/630635.png"}
+        title={card.lotto_name}
+        countDownText={"เหลือเวลาอีก 0 วัน 00:00:00"}
+        details={card.description}
+        open={card.is_active}
+        name={'test'}
+        agent={card.lotto_name}
       />
     {/each}
   {/if}
